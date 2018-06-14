a31921327b5955f8dce20bd476b6f69bb87b6f37
Exorust <chandrahas.aroori@gmail.com>
2018-06-14 21:48:36 +0530

    Next

--- Day4.cpp
+++ Day4.cpp
@@ -12,17 +12,32 @@ int main()
   cin.tie(NULL);
   ifstream infile;
   infile.open("inp");
+  string s;
+  char c;
+  set<string> us;
+  int count=0;
+  int internal_count=0;
   while(infile.get(c) ){
     if(c =='\n') {
-
+      us.insert(s);
+      internal_count++;
+      s.clear();
+      if(internal_count == us.size()) {
+        count++;
+      }
+      internal_count=0;
+      us.clear();
     }
     else if(c=='\t' || c==' ') {
-
+      us.insert(s);
+      s.clear();
+      internal_count++;
     }
     else {
-
+      s.push_back(c);
     }
   }
+  cout<<count<<endl;
 
 	return 0;
 }
